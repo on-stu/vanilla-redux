0af4ec7782d56b589bc6a53e79f8abbd2a753820
onstu <minsu0523@naver.com>
2021-07-14 20:49:26 +0900

    Vanilla JS TodoList with Redux setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+import { createStore } from 'redux';
+
 const form = document.querySelector("form");
-const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 const input = document.querySelector("input");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type){
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 const createTodo = (todo) => {
   const li = document.createElement("li");
@@ -13,7 +30,7 @@ const onSubmit = (e) => {
   e.preventDefault();
   const todo = input.value;
   input.value = "";
-  createTodo(todo);
+  store.dispatch({type: ADD_TODO, text: todo})
 };
 
 form.addEventListener("submit", onSubmit)
