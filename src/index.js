4461ca569e5e0644fbf24bbb21c86bd83b0e1ade
onstu <minsu0523@naver.com>
2021-07-14 13:28:50 +0900

    Counter with Redux 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let counter = 0;
-
-number.innerText = counter;
-
-const updateCount = () => {
-  number.innerText = counter;
-}
-
-const handlePlus = () => {
-  counter = counter + 1;
-  updateCount();
+const countModifier = (count = 0) => {  //initialize default value
+  return count;  // count는 state 형식을 가진다
 };
 
-const handleMinus = () => {
-  counter = counter - 1;
-  updateCount();
-};
 
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState())
+
+
+// let counter = 0;
+
+// number.innerText = counter;
+
+// const updateCount = () => {
+//   number.innerText = counter;
+// }
+
+// const handlePlus = () => {
+//   counter = counter + 1;
+//   updateCount();
+// };
+
+// const handleMinus = () => {
+//   counter = counter - 1;
+//   updateCount();
+// };
+
+// plus.addEventListener("click", handlePlus);
+// minus.addEventListener("click", handleMinus);
